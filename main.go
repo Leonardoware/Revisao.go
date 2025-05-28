3747deb8e3103edf53b735c19ce6723871607b36
Leonardo <leonardo.a.aguiar@aluno.senai.br>
2025-05-28 15:58:11 -0300

    Maior e menor de idade

--- main.go
+++ main.go
@@ -3,25 +3,18 @@ package main
 import "fmt"
 
 func main(){
-	Produtos := make(map[string]int)
+	var nome string
+	var idade int
 
-	Produtos["Coxinha"] += 10
-	Produtos["Pão de Queijo"] += 15
-	Produtos["Refrigerante"] += 20
-
-	for produto, quantidade := range Produtos{
-		fmt.Println(produto, quantidade)
+	fmt.Print("Digite nome e idade:")
+	fmt.Scan(&nome, &idade)
+	fmt.Println("Nome:", nome)
+	fmt.Println("Idade:", idade)
+	
+	if idade >= 18{
+		fmt.Print("Status: Maior de idade")
+	} else {
+		fmt.Print("Status:Menor de idade")
 	}
 
-	fmt.Println("\nApós a venda:\n")
-	
-	posVenda := make(map[string]int)
-	
-	posVenda["Coxinha"] += 8
-	posVenda["Pão de Quejo"] += 14
-	posVenda["Refrigerante"]+= 20
-
-	for produtos2, quantiaAtual := range posVenda{
-		fmt.Println(produtos2, quantiaAtual)
-	}
 }
