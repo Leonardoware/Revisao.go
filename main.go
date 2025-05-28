990bfa9f0589baeb7240a8a6229da555872dc561
Leonardo <leonardo.a.aguiar@aluno.senai.br>
2025-05-28 15:56:56 -0300

    For

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
-var a int = 3
-var nome string = "Leo"
-const c = 4
-
 func main(){
-	
-	b := 2
-	n := "Leonard"
+	Produtos := make(map[string]int)
 
-	fmt.Print("Os 3 números são:", a , b, c)
-	fmt.Print("Os nomes são:", nome, n)
+	Produtos["Coxinha"] += 10
+	Produtos["Pão de Queijo"] += 15
+	Produtos["Refrigerante"] += 20
+
+	for produto, quantidade := range Produtos{
+		fmt.Println(produto, quantidade)
+	}
+
+	fmt.Println("\nApós a venda:\n")
+	
+	posVenda := make(map[string]int)
+	
+	posVenda["Coxinha"] += 8
+	posVenda["Pão de Quejo"] += 14
+	posVenda["Refrigerante"]+= 20
+
+	for produtos2, quantiaAtual := range posVenda{
+		fmt.Println(produtos2, quantiaAtual)
+	}
 }
